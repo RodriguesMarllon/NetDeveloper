4fdcaa89c8d96a32fddc83cd6e514faa218bc421
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-15 18:39:34 -0300

    Validadndo a propriedade idade

--- 02 - Explorando a linguagem C#/ExemploExplorando/Models/Pessoa.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/Models/Pessoa.cs
@@ -8,6 +8,7 @@ namespace ExemploExplorando.Models
     public class Pessoa
     {
         private string _nome;
+        private int _idade;
         public string Nome
         {
             get => _nome.ToUpper();
@@ -23,7 +24,20 @@ namespace ExemploExplorando.Models
             }
         }
 
-        public int Idade { get; set; }
+        public int Idade
+        {
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("a idade não pode ser menor do que zero");
+                }
+
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
--- 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9970f09d004aa54425a64dbda2159a1dcb87fcdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767e2ed60f8b8543793f4ca9501047ae066ee2e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
