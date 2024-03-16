98d7acedb0bd2d41d63a6bb1d9b035ce480adb47
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-15 22:30:44 -0300

    Entendendo o funcionamento das Pilhas

--- 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac217483bf16244151e36524b1e07ac4af3dd5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce35f5cf187b38e893c6002bee9f384f599510d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
