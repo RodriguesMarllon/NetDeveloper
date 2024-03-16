a172c61e053b3f9a687d66f543b04a11256422e8
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-16 09:18:05 -0300

    Descartes

--- 02 - Explorando a linguagem C#/ExemploExplorando/Program.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/Program.cs
@@ -2,15 +2,53 @@
 using System.Globalization;
 using System.Runtime.CompilerServices;
 
-(int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Leonardo", "Buta", 10.5M);
+LeituraArquivo arquivo = new LeituraArquivo();
 
-ValueTuple<int, string, string, decimal> outroExemplo = (1, "Leonardo", "Buta", 1.80M);
-var outroExemploTuplaCreate = Tuple.Create(1, "Leonardo", "Buta", 1.80M);
+var (sucesso, linhasArquivo, _) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
 
-Console.WriteLine($"Id: {tupla.Item1}");
-Console.WriteLine($"Nome: {tupla.Item2}");
-Console.WriteLine($"Sobrenome: {tupla.Item3}");
-Console.WriteLine($"Altura: {tupla.Item4}");
+if (sucesso)
+{
+    // Console.WriteLine("Quantidade de linhas do arquivo: " + QuantidadeLinhas);
+    foreach (string linha in linhasArquivo)
+    {
+        Console.WriteLine(linha);
+    }
+}
+else
+{
+    Console.WriteLine("Não foi possível ler o arquivo");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Leonardo", "Buta", 10.5M);
+
+// ValueTuple<int, string, string, decimal> outroExemplo = (1, "Leonardo", "Buta", 1.80M);
+// var outroExemploTuplaCreate = Tuple.Create(1, "Leonardo", "Buta", 1.80M);
+
+// Console.WriteLine($"Id: {tupla.Item1}");
+// Console.WriteLine($"Nome: {tupla.Item2}");
+// Console.WriteLine($"Sobrenome: {tupla.Item3}");
+// Console.WriteLine($"Altura: {tupla.Item4}");
 
 
 
--- 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b5deef93ec0ac806d481a6a8c165d4281540ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf67a9a20e8d9f918ca108bbc7715afa95443a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
