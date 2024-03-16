6f73a6b99dd927341083eecd587c1018cdcb0d4a
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-16 10:17:31 -0300

    DateTime em JSON

--- 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8872447fa3a80ec81aaca1973b2d86d06bfb579d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f03d9ffe88a773e56d5444da6e4bc1c64140ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
