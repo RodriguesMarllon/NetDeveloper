dce35f5cf187b38e893c6002bee9f384f599510d
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-15 22:24:48 -0300

    Entendendo o funcionamento de Filas

--- 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ 02 - Explorando a linguagem C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc61698a5d6b4f9ac167468c37faa9c6f9bd7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac217483bf16244151e36524b1e07ac4af3dd5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
