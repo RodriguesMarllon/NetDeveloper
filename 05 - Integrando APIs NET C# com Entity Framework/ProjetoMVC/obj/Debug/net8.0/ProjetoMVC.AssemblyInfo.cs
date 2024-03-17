ca3d27c564374741bb2ecb8cf3d10f26a5fc0898
Desktop Marllon <marllonrsantos@gmail.com>
2024-03-17 20:02:02 -0300

    Criando as migrations

--- 05 - Integrando APIs NET C# com Entity Framework/ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
+++ 05 - Integrando APIs NET C# com Entity Framework/ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7d7723d519aefb8344aca1745c1bcfb80e63f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c428d17025ef6270379a78e67b724182d331a59e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
