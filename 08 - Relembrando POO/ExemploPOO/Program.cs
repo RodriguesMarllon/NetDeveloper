11eef0eda63aa9d49cad61888a61d0362c5d8765
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-07-28 22:04:40 -0300

    FEAT: Classe abstrata na prática

--- 08 - Relembrando POO/ExemploPOO/Program.cs
+++ 08 - Relembrando POO/ExemploPOO/Program.cs
@@ -1,20 +1,6 @@
 ﻿using ExemploPOO.Models;
 
-Aluno a1 = new Aluno
-{
-    Nome = "Marllon",
-    Idade = 19,
-    Email = "marllon@gmail.com",
-    Nota = 10
-};
-a1.Apresentar();
+Corrente c = new Corrente();
 
-
-Professor p1 = new Professor
-{
-    Nome = "Eduardo",
-    Idade = 30,
-    Email = "eduardo@gmail.com",
-    Salario = 1000
-};
-p1.Apresentar();
+c.Creditar(500);
+c.ExibirSaldo();
